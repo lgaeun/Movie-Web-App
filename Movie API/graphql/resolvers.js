c27a8fcffb5307ba4cb8ff00cc42037da40425c9
lgaeun <gaeungaeun4837@gmail.com>
2021-03-10 11:03:49 +0900

    0.6 Extending the Schema - Schema에 새로운 type 정의

--- Movie API/graphql/resolvers.js
+++ Movie API/graphql/resolvers.js
@@ -1,7 +1,13 @@
 // resolvers: something that resolves a query
+const nicolas = {
+  name: "Nicolas",
+  age: 18,
+  gender: "female",
+};
+
 const resolvers = {
   Query: {
-    name: () => "nicolas", //어떤 사용자가 name query를 보내면 'nicolas'를 반환하는 함수로 답할 것.
+    person: () => nicolas, //어떤 사용자가 name query를 보내면 'nicolas'를 반환하는 함수로 답할 것.
   },
 };
 
