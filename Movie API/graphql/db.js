b91c28b22eb2f62c994c3a35fd5661363552b963
lgaeun <gaeungaeun4837@gmail.com>
2021-03-12 11:09:43 +0900

    0.8 argument를 받아 쿼리 생성하기

--- Movie API/graphql/db.js
+++ Movie API/graphql/db.js
@@ -31,8 +31,10 @@ export const people = [
   },
 ];
 
+// DB를 통해 하고 싶은 함수들 정의
+
 export const getById = (id) => {
   //요청된id(파라미터로 넘어온)가 people 배열의 id와 일치하는 것을 filteredPeople에 저장
-  const filteredPeople = people.filter((person) => id === people.id);
+  const filteredPeople = people.filter((person) => person.id === String(id));
   return filteredPeople[0]; // filter된 것들 중 첫 번째 것을 리턴
 };
