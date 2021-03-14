450c9ece80018081d0ae70920a23fe83c0c8e9dd
lgaeun <gaeungaeun4837@gmail.com>
2021-03-14 23:44:26 +0900

    person -> moive, mutation resolver에 정의

--- Movie API/graphql/db.js
+++ Movie API/graphql/db.js
@@ -1,40 +1,48 @@
-export const people = [
+let movies = [
   {
     id: "1",
-    name: "Nicolas",
-    age: 18,
-    gender: "female",
+    name: "Avengers",
+    score: 5,
   },
   {
     id: "2",
     name: "Emma",
-    age: 18,
-    gender: "female",
+    score: 4,
   },
   {
     id: "3",
-    name: "Kaia",
-    age: 18,
-    gender: "female",
+    name: "Imitation Game",
+    score: 5,
   },
   {
     id: "4",
-    name: "Flynn",
-    age: 18,
-    gender: "male",
-  },
-  {
-    id: "5",
-    name: "Alec",
-    age: 18,
-    gender: "male",
+    name: "How to Traian Your Dragon",
+    score: 4,
   },
 ];
 
 // DB를 통해 하고 싶은 함수들 정의
+export const getMoives = () => movies;
 
 export const getById = (id) => {
   //요청된id(파라미터로 넘어온)가 people 배열의 id와 일치하는 것을 filteredPeople에 저장
-  const filteredPeople = people.filter((person) => person.id === String(id));
-  return filteredPeople[0]; // filter된 것들 중 첫 번째 것을 리턴
+  const filteredMoives = movies.filter((movie) => movie.id === String(id));
+  return filteredMovies[0]; // filter된 것들 중 첫 번째 것을 리턴
+};
+
+//define mutation
+export const deleteMovie = (id) => {
+  const cleanedMoives = movies.filter((movie) => movie.id !== String(id));
+  if (movies.length > cleanedMoives.length) {
+    movies = cleanedMoives;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMoive = (name, score) => {
+  const newMovie = {
+    //not yet defined
+  };
 };
--- Movie API/graphql/resolvers.js
+++ Movie API/graphql/resolvers.js
@@ -1,13 +1,16 @@
 // resolvers: something that resolves a query
-import { getById, people } from "./db";
+import { addMoive, getById, getMoives } from "./db";
 
 //resolvers can be anything - 어떤 일을 실행시키고 싶은지 정의! (다른 api 가기, DB접근 등등)
 const resolvers = {
   Query: {
     // name: () => "nicolas", //어떤 사용자가 name query를 보내면 'nicolas'를 반환하는 함수로 답할 것.
-    people: () => people,
-    person: (_, { id }) => getById(id), // 사용자로부터 id 얻어오기
+    movies: () => getMoives(),
+    movie: (_, { id }) => getById(id), // 사용자로부터 id 얻어오기
   },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMoive(name, score);
+  }
 };
 
 export default resolvers;
