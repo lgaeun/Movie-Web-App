cb6916eba80a27494e2a3ebb99228ede94db0e73
lgaeun <gaeungaeun4837@gmail.com>
2021-04-08 00:18:42 +0900

    1.1Get MOVIES query

--- Movie Client/src/routes/Home.js
+++ Movie Client/src/routes/Home.js
@@ -1,2 +1,28 @@
 import React from "react";
-export default () => "Home";
+import {gql} from "apollo-boost";
+import { useQuery } from "@apollo/client";
+
+//copy the query
+const GET_MOVIES = gql`
+    { 
+        movies{
+            id
+            medium_cover_image
+        }
+    }
+`;
+
+export default () => {
+    const{ loading, error,data } = useQuery(GET_MOVIES);  //use query - react hook
+    // console.log(loading, error, data);
+    if(loading){
+        return "loading...";
+    }
+    if(data && data.movies){
+        return data.movies.map(m => <h1>{m.id}</h1>);
+    }
+    if(error){
+        console.log(error);
+    }
+    return <h1>Home</h1>;
+};
